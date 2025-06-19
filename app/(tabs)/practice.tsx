8f862448f65137dde10b783ed7ef3e00d582ecf6
Nikhil Anand <nikhil.anand7@s.amity.edu>
2025-06-19 09:41:08 +0530

    Some styling changes to navigation

--- app/(tabs)/practice.tsx
+++ app/(tabs)/practice.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function practice() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>practice</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- app/(tabs)/tipsAndTricks.tsx
+++ app/(tabs)/tipsAndTricks.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function tipsAndTricks() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>tipsAndTricks</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
