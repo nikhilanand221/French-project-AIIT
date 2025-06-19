1825091e6eeb7422985953de9fb5c892858cdfd8
Nikhil Anand <nikhil.anand7@s.amity.edu>
2025-06-19 08:25:49 +0530

    basic homescreen

---
+++ app/(tabs)/tipsAndTricks.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function tipsAndTricks() {
+  return (
+    <View>
+      <Text>tipsAndTricks</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
